78962ada441e7df95a6be2ea7eca9505ddce92fa
Burkey Koontz <bk.koontz@gmail.com>
2018-07-16 11:49:36 -0700

    added a new variable and console.log statement

--- firstfile.js
+++ firstfile.js
@@ -2,7 +2,9 @@ console.log("Hi, my name is jaouad and I'm 29 years old");
 
 function whoAmI(name, age) {
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    let yearOfBirth = 2018 - age;
+    console.log(`I was born in ${yearOfBirth}`);
 }
 
-whoAmI("Jaouad", "unknown");
+whoAmI("Jaouad", 30);
 
