d2cc72f007e516b31becb04fb411497ba65c4ef9
Burkey Koontz <bk.koontz@gmail.com>
2018-07-16 11:47:37 -0700

    Added parameters

--- firstfile.js
+++ firstfile.js
@@ -1,7 +1,8 @@
 console.log("Hi, my name is jaouad and I'm 29 years old");
 
-function whoAmI() {
-    console.log("Hi, my name is jaouad and I'm 29 years old");
+function whoAmI(name, age) {
+    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 }
 
-whoAmI();
+whoAmI("Jaouad", "unknown");
+
